859cc367a3b6dd52c50a6c012b6a4de06316db7b
Ivan Díaz <correo@ivandiazdiaz.com>
2022-08-28 22:09:54 +0200

    kickoff

---
+++ Readme.cmd
@@ -0,0 +1,11 @@
+ionic capacitor add android
+ionic capacitor copy android
+
+ionic capacitor run android -l --host=192.168.1.37
+
+
+chrome://inspect/#devices
+
+build android
+ionic cordova prepare android
+ionic cordova bild android
